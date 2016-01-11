435166d02c187121a0154761ab155a8dec4ec014
M.Yasoob Ullah Khalid ☺ <yasoob.khld@gmail.com>
2016-01-11 23:07:49 +0500

    login and callback working. :heart:

--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 //Lets require/import the HTTP module
 var http = require('http');
 var dispatcher = require('httpdispatcher');
+var config = require('./config.js');
 
-//Lets define a port we want to listen to
+const HOST = 'localhost';
 const PORT=8080; 
 
-//We need a function which handles requests and send response
+var GitHubConfig = {
+	'client_id'   : config['client_id'],
+	'secret'      : config['secret'],
+	'redirect_uri': '',
+	'scope'       : '',
+	'state'       : Math.round(Math.random()*10)
+}
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
+
+//A sample GET request    
+dispatcher.onGet("/", function(req, res) {
+    res.writeHead(200);
+    res.write("<!DOCTYPE html><html><body><a href='login'>Login!</a></body></html>");
+    res.end();
+});    
+
+dispatcher.onGet("/login", function(req, res) {
+  var url = 'https://github.com/login/oauth/authorize'
+  + '?client_id=' + GitHubConfig.client_id
+  + (GitHubConfig.scope ? '&scope=' + GitHubConfig.scope : '')
+  + '&state=' + GitHubConfig.state;
+
+  res.setHeader('location', url);
+  res.statusCode = 302;
+  res.end();
+});    
+
+
+dispatcher.onGet("/callback", function(req, res) {
+  res.write("Lolipop");
+  res.end();
+});
+
+
+//A sample POST request
+dispatcher.onPost("/a", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
+
+//Lets use our dispatcher
 function handleRequest(request, response){
-    //response.end('It Works!! Path Hit: ' + request.url);
-    response.end('It Works!! Path Hit: ' + request.url);
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 //Create a server
